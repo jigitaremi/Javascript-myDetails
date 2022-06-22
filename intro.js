faafecb8983da51947166089e31bd429a02264d6
Oluwaremilekun Aluko <105178485+jigitaremi@users.noreply.github.com>
2022-06-22 15:21:58 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,6 @@
+let myName = "Aluko Oluwaremilekun" 
+let height = "5.7ft"
+let country = "Nigeria"
+
+
+alert("My name is " + myName + "." + " My height is " + height + " and my country is " + country + ".")
